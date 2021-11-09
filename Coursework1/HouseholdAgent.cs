c1e68e89c0f35d47213743dce0a610576764a8a4
rorymckenzie <rorymckenzie@mailbox.org>
2021-11-09 17:16:58 +0000

    HouseholdAgent now sets variables based on message and prints to console

--- Coursework1/HouseholdAgent.cs
+++ Coursework1/HouseholdAgent.cs
@@ -9,6 +9,12 @@ namespace Coursework1
 {
     class HouseholdAgent : Agent
     {
+
+        private int dailyNeed;
+        private int dailyGenerate;
+        private int utilityBuyPrice;
+        private int utilitySellPrice;
+
         public override void Setup()
         {
             Send("environment", "start");
@@ -21,7 +27,22 @@ namespace Coursework1
 
         public override void Act(Message message)
         {
-            //Console.WriteLine(message.Format());
+            string messageC = message.Content;
+
+            string[] words = messageC.Split(' ');
+
+            switch (words[0])
+            {
+                case "inform":
+                    dailyNeed = Int32.Parse(words[1]);
+                    dailyGenerate = Int32.Parse(words[2]);
+                    utilityBuyPrice = Int32.Parse(words[3]);
+                    utilitySellPrice = Int32.Parse(words[4]);
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(message.Format() + "\n" + this.Name + " - Needs:" + dailyNeed.ToString() + "kWh, Generates: " + dailyGenerate.ToString() + "kWh, BuyPrice: £" + utilityBuyPrice.ToString() + ", SellPrice: £" + utilitySellPrice.ToString());
         }
     }
 }
--- Coursework1/Program.cs
+++ Coursework1/Program.cs
@@ -16,6 +16,12 @@ namespace Coursework1
             var e = new EnvironmentAgent(); env.Add(e, "environment");
 
             var h1 = new HouseholdAgent(); env.Add(h1, "agent1");
+            var h2 = new HouseholdAgent(); env.Add(h2, "agent2");
+            var h3 = new HouseholdAgent(); env.Add(h3, "agent3");
+            var h4 = new HouseholdAgent(); env.Add(h4, "agent4");
+            var h5 = new HouseholdAgent(); env.Add(h5, "agent5");
+            var h6 = new HouseholdAgent(); env.Add(h6, "agent6");
+
 
             env.Start();
             Console.ReadLine();
