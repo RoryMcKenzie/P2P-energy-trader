7e9f9ebe48ca5d000dbb59975f4dbb33b9ef00e0
rorymckenzie <rorymckenzie@mailbox.org>
2021-11-09 16:48:05 +0000

    HouseholdAgent instance now sends start to EnvironmentAgent instance

--- Coursework1/HouseholdAgent.cs
+++ Coursework1/HouseholdAgent.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ActressMas;
 
 namespace Coursework1
 {
-    class HouseholdAgent
+    class HouseholdAgent : Agent
     {
+        public override void Setup()
+        {
+            Send("environment", "start");
+        }
+
+        public override void ActDefault()
+        {
+            
+        }
+
+        public override void Act(Message message)
+        {
+            //Console.WriteLine(message.Format());
+        }
     }
 }
--- Coursework1/Program.cs
+++ Coursework1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ActressMas;
 
 namespace Coursework1
 {
@@ -10,6 +11,14 @@ namespace Coursework1
     {
         static void Main(string[] args)
         {
+
+            var env = new EnvironmentMas(noTurns: 100);
+            var e = new EnvironmentAgent(); env.Add(e, "environment");
+
+            var h1 = new HouseholdAgent(); env.Add(h1, "agent1");
+
+            env.Start();
+            Console.ReadLine();
         }
     }
 }
