277c8df41c7229ecaf58e9a956f8101362305555
RoryMcKenzie <rorymckenzie212@gmail.com>
2021-11-11 15:48:23 +0000

    HouseholdAgent is now assigned either buyer or seller, made BrokerAgent and ProviderAgent classes

--- Coursework1/HouseholdAgent.cs
+++ Coursework1/HouseholdAgent.cs
@@ -14,6 +14,10 @@ namespace Coursework1
         private int dailyGenerate;
         private int utilityBuyPrice;
         private int utilitySellPrice;
+        private bool isBuyer = false;
+        private bool isSeller = false;
+        private int amountToBuy = 0;
+        private int amountToSell = 0;
 
         public override void Setup()
         {
@@ -38,11 +42,36 @@ namespace Coursework1
                     dailyGenerate = Int32.Parse(words[2]);
                     utilityBuyPrice = Int32.Parse(words[3]);
                     utilitySellPrice = Int32.Parse(words[4]);
+                    if (dailyGenerate > dailyNeed)
+                    {
+                        isSeller = true;
+                        amountToSell = dailyGenerate - dailyNeed;
+                    }
+                    else
+                    {
+                        isBuyer = true;
+                        amountToBuy = dailyNeed - dailyGenerate;
+                    }
                     break;
                 default:
                     break;
             }
-            Console.WriteLine(message.Format() + "\n" + this.Name + " - Needs:" + dailyNeed.ToString() + "kWh, Generates: " + dailyGenerate.ToString() + "kWh, BuyPrice: £" + utilityBuyPrice.ToString() + ", SellPrice: £" + utilitySellPrice.ToString());
+
+            String x;
+
+            if (isSeller)
+            {
+                x = "seller";
+            }
+            else if (isBuyer)
+            {
+                x = "buyer";
+            }
+            else
+            {
+                x = "???";
+            }
+            Console.WriteLine(message.Format() + "\n" + this.Name + " - Needs:" + dailyNeed.ToString() + "kWh, Generates: " + dailyGenerate.ToString() + "kWh, BuyPrice: £" + utilityBuyPrice.ToString() + ", SellPrice: £" + utilitySellPrice.ToString() + " " + x);
         }
     }
 }
---
+++ Coursework1/ProviderAgent.cs
@@ -0,0 +1,7 @@
+namespace Coursework1
+{
+    public class ProviderAgent
+    {
+        
+    }
+}
